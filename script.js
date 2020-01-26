8b26624cd7158b5404f4ab77b5116a08cf29460e
jeffbartelli <jeff.bartelli@gmail.com>
2020-01-26 14:37:31 -0800

    1-26- js clone script completed

--- script.js
+++ script.js
@@ -1368,7 +1368,7 @@ let dataSources = [
     section:"profile",
     id:"metroPop",
     metricName:"Metropolitan Population",
-    description:,
+    description:null,
     source:"https://en.wikipedia.org/wiki/List_of_metropolitan_statistical_areas",
     score:null,
     scoreLabel:null,
@@ -1422,6 +1422,7 @@ let dataSources = [
 ]
 const rankOrder = [null,null,0,0,0,1,0,1,0,0,0,1,1,0,0,0,0,1,1,0,1,1,1,1,0,1,1,1,1,1];
 
+// SCRIPT FOR SORTING AND RANKING EACH METRIC ACROSS ALL THE CITIES AND ADDING THE RANK TO EACH CITY'S KEYS.
 const dataKeys = Object.keys(data[0]);
 let temp = [];
 let testSort = [];
@@ -1458,3 +1459,79 @@ for(let j=0;j<dataKeys.length;j++){
 //   }
 // });
 // console.log(count);
+
+// INDIVIDUAL METRIC NODE FOR CLONING
+let metric = document.createElement('div');
+metric.setAttribute('class','metric');
+let metricForm = document.createElement('form');
+metricForm.setAttribute('action','');
+metricForm.setAttribute('class','');
+metric.appendChild(metricForm);
+const top1 = document.createElement('div');
+top1.setAttribute('class','top');
+metricForm.appendChild(top1);
+const expand = document.createElement('span');
+expand.setAttribute('class','expand');
+expand.innerHTML = '+';
+top1.appendChild(expand);
+const name = document.createElement('label');
+name.setAttribute('class','name');
+name.innerHTML = '[Metric Name]';
+top1.appendChild(name);
+const switcher = document.createElement('label');
+switcher.setAttribute('class','switch');
+top1.appendChild(switcher);
+const checkbox = document.createElement('input');
+checkbox.setAttribute('type','checkbox');
+checkbox.setAttribute('onchange','');// update this to point to a function that adjusts this value in dataSources.
+checkbox.setAttribute('value','1');
+switcher.appendChild(checkbox);
+const slider = document.createElement('span');
+slider.setAttribute('class','slider round');
+switcher.appendChild(slider);
+const number = document.createElement('input');
+number.setAttribute('type','number');
+number.setAttribute('value','100');
+number.setAttribute('max','100');
+number.setAttribute('min','0');
+number.setAttribute('maxlength','3');
+number.setAttribute('onchange','');//update this to point to the appropriate function
+number.setAttribute('class','weight');
+number.setAttribute('required','');
+top1.appendChild(number);
+const mid = document.createElement('div');
+mid.setAttribute('class','mid');
+metricForm.appendChild(mid);
+const description = document.createElement('label')
+description.setAttribute('class','description');
+description.innerHTML = 'Description: []';
+mid.appendChild(description);
+const bottom = document.createElement('div');
+bottom.setAttribute('class','bottom');
+metricForm.appendChild(bottom);
+const source = document.createElement('label');
+source.setAttribute('class','source');
+source.innerHTML = 'Source';
+bottom.appendChild(source);
+let sourceLink = document.createElement('a');
+sourceLink.setAttribute('href','#');
+source.appendChild(sourceLink);
+const top10 = document.createElement('span');
+top10.setAttribute('class','top10');
+top10.innerHTML = '?';
+bottom.appendChild(top10);
+const rank = document.createElement('label');
+rank.setAttribute('class','rank');
+rank.innerHTML = 'Rank: '
+bottom.appendChild(rank);
+const rankVal = document.createElement('span');
+rankVal.innerHTML = '[]';
+rank.appendChild(rankVal);
+const score = document.createElement('label');
+score.setAttribute('class','score');
+score.innerHTML = '[score]';
+bottom.appendChild(score);
+
+let metricBlock = metric.cloneNode(true);
+let sunshine = document.querySelector('#sunshine').appendChild(metricBlock);
+let precip = document.querySelector('#precip');
